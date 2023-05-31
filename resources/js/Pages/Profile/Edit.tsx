194872a38b0d82a8542872499b8bca3fab43a191
DamianBigos <damianbigos1500@gmail.com>
2023-05-31 19:27:40 +0200

    inertia-breeze: change files to tsx

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -4,7 +4,7 @@ import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 import { Head } from '@inertiajs/react';
 
-export default function Edit({ auth, mustVerifyEmail, status }) {
+export default function Edit({ auth, mustVerifyEmail, status }:any) {
     return (
         <AuthenticatedLayout
             user={auth.user}
